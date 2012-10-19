57c7f4108bfffbe2d08ea24b877223f85eac467d
Naresh Bhatia <naresh.archfirst@gmail.com>
2012-10-19 01:28:59 -0400

    Added 5 initial grid implementations.

---
+++ jquery-mobile-html-table/js/main.js
@@ -0,0 +1,68 @@
+$(document).ready(function() {
+
+    // Register Handlebar helpers
+    Handlebars.registerHelper('formatMoney', function(amount) {
+        return (amount >= 0) ?
+            '$' + $.format.number(amount, '#,##0.00') :
+            '($' + $.format.number(-amount, '#,##0.00') + ')';
+    });
+
+    Handlebars.registerHelper('formatPercent', function(percent) {
+        return (percent >= 0) ?
+            $.format.number(percent, '#,##0.00') + '%' :
+            '(' + $.format.number(-percent, '#,##0.00') + '%)';
+    });
+
+    Handlebars.registerHelper('getSign', function(number) {
+        return (number >= 0) ? 'positive' : 'negative';
+    });
+
+    // Compile handlebar template
+    var src = $('#positionTemplate').html();
+    var template = Handlebars.compile(src);
+
+    // Compute position values
+    $.each(positions, function(index, position) {
+        position.marketValue = position.lastTrade * position.quantity;
+        position.totalCost = position.pricePaid * position.quantity;
+        position.gain = position.marketValue - position.totalCost;
+        position.gainPercent = (position.gain/position.totalCost) * 100;
+    });
+
+    // Render table
+    var table = $('#positions-table tbody');
+    $.each(positions, function(index, position) {
+        table.append(template(position));
+    });
+
+    // Display window size on resize events
+    function displayWindowSize() {
+        var win = $(this);
+        $('.window-size').html("(" + win.width() + ", " + win.height() + ")");
+    }
+    $(window).resize(displayWindowSize);
+
+    // Show selection on click events
+    $('#positions-table tbody tr').click(function() {
+        var security = $(this).find('.security').html();
+        $('#selected-position').html(security);
+    });
+
+    // Fit table on resize events
+    var fixedSectionsHeight =
+        $('#positions-header').height() +
+        $('#selected-position').height() +
+        $('#layout-info').height()
+        + 70;  // fudge-factor
+
+    function fitTable() {
+        var win = $(this);
+        $('#positions-table-container').height(
+            win.height() - fixedSectionsHeight);
+    }
+    $(window).resize(fitTable);
+
+    // Perform initial setup
+    displayWindowSize();
+    fitTable();
+});
---
+++ juqery-mobile-jqgrid/js/main.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+
+    // Compute position values
+    $.each(positions, function(index, position) {
+        position.marketValue = position.lastTrade * position.quantity;
+        position.totalCost = position.pricePaid * position.quantity;
+        position.gain = position.marketValue - position.totalCost;
+        position.gainPercent = (position.gain/position.totalCost) * 100;
+    });
+
+    // Render table
+    $('#positions-table').jqGrid({
+   	    colNames:['Security','Symbol', 'Quantity', 'Last Trade','Market Value','Price Paid','Total Cost','Gain','Gain %'],
+   	    colModel:[
+   		    {name:'security',index:'security', width:200},
+   		    {name:'symbol',index:'symbol', width:50},
+   		    {name:'quantity',index:'quantity', width:80},
+   		    {name:'lastTrade',index:'lastTrade', width:80, align:"right"},
+   		    {name:'marketValue',index:'marketValue', width:80, align:"right"},		
+   		    {name:'pricePaid',index:'pricePaid', width:80,align:"right"},		
+   		    {name:'totalCost',index:'totalCost', width:80, sortable:false},
+   		    {name:'gain',index:'gain', width:80, sortable:false},		
+   		    {name:'gainPercent',index:'gainPercent', width:80, sortable:false}		
+   	    ],
+        datatype: 'local',
+        data: positions,
+        rowNum: 10000 /* this is a hack to workaround jqGrid bug */
+    });
+
+
+    // Display window size on resize events
+    function displayWindowSize() {
+        var win = $(this);
+        $('.window-size').html("(" + win.width() + ", " + win.height() + ")");
+    }
+    $(window).resize(displayWindowSize);
+
+    // Show selection on click events
+    $('#positions-table tbody tr').click(function() {
+        var security = $(this).find('.security').html();
+        $('#selected-position').html(security);
+    });
+
+    // Fit table on resize events
+    var fixedSectionsHeight =
+        $('#positions-header').height() +
+        $('#selected-position').height() +
+        $('#layout-info').height()
+        + 95;  // fudge-factor
+
+    function fitTable() {
+        var win = $(this);
+        $('#positions-table').setGridWidth(win.width() - 35, true);
+        $('#positions-table').setGridHeight(
+            win.height() - fixedSectionsHeight);
+    }
+    $(window).resize(fitTable);
+
+    // Perform initial setup
+    displayWindowSize();
+    fitTable();
+});
