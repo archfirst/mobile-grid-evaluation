270faee340f7e04a7be1ff9bff42f1274fbc5d9e
SRAVAN KUMAR REKANDAR <srekandar@sapient.com>
2012-12-26 20:31:32 +0530

    Linted

--- jquery-mobile-slickgrid/js/main.js
+++ jquery-mobile-slickgrid/js/main.js
@@ -1,7 +1,7 @@
-(function ($) {
-    "use strict";
+(function ($, undefined) {
+    'use strict';
 
-    var grid, data, columns, options, columnWidths;
+    var grid, data, columns, options;
 
     data = positions || []; // get from ../data/positions.js
     // Compute position values
@@ -17,41 +17,54 @@
     function formatNumber(amount) {
         return $.format.number(amount, '#,##0.00');
     }
-    function formatCurrency(row, cell, value, columnDef, dataContext) {
-        return "$" + formatNumber(value);
+	
+	/* Full List of Arguments(row, cell, value, columnDef, dataContext)*/
+    function formatCurrency(row, cell, value) {
+        return '$' + formatNumber(value);
     }
-    function formatGainMoney(row, cell, value, columnDef, dataContext) {
+	
+	/* Full List of Arguments(row, cell, value, columnDef, dataContext) */
+    function formatGainMoney(row, cell, value) {
         var formattedValue = (value < 0) ?
                             formatNumber(-1 * value) :
                             formatNumber(value);
 
         if (value < 0) {
-            return "<span class='negative'>$(" + formattedValue + ")</span>";
+            return '<span class="negative">$(' + formattedValue + ')</span>';
         }
 
-        return "$" + formattedValue;
+        return '$' + formattedValue;
     }
-    function formatGainPercent(row, cell, value, columnDef, dataContext) {
+	
+	/* Full List of Arguments(row, cell, value, columnDef, dataContext) */
+    function formatGainPercent(row, cell, value) {
         var formattedValue = (value < 0) ?
                             formatNumber(-1 * value) :
                             formatNumber(value);
         if (value < 0) {
-            return "<span class='negative'>(" + formattedValue + "%)</span>";
+            return '<span class="negative">(' + formattedValue + '%)</span>';
         }
-        return formattedValue + "%";
+        return formattedValue + '%';
     }
 
     // Preparing the Columns
     columns = [
-        {id: "security", name: "Security", field: "security", width: 200, resizable: false, headerCssClass: "left-align", minWidth: 300},
-        {id: "symbol", name: "Symbol", field: "symbol", cssClass: "center-align", maxWidth: 80},
-        {id: "quantity", name: "Quantity", field: "quantity", cssClass: "right-align", headerCssClass: "right-align"},
-        {id: "last-trade", name: "Last Trade", field: "lastTrade", cssClass: "right-align", headerCssClass: "right-align",  formatter: formatCurrency},
-        {id: "market-value", name: "Market Value", field: "marketValue", cssClass: "right-align", headerCssClass: "right-align", formatter: formatCurrency},
-        {id: "price-paid", name: "Price Paid", field: "pricePaid", cssClass: "right-align", headerCssClass: "right-align", formatter: formatCurrency},
-        {id: "total-cost", name: "Total Cost", field: "totalCost", cssClass: "right-align", headerCssClass: "right-align", formatter: formatCurrency},
-        {id: "gain", name: "Gain", field: "gain", cssClass: "positive right-align", headerCssClass: "right-align", formatter: formatGainMoney},
-        {id: "gain-percent", name: "Gain %", field: "gainPercent", cssClass: "positive right-align", headerCssClass: "right-align", formatter: formatGainPercent}
+        {id: 'security', name: 'Security', field: 'security', width: 200, resizable: false, headerCssClass: 'left-align', minWidth: 300},
+        {id: 'symbol', name: 'Symbol', field: 'symbol', cssClass: 'center-align', maxWidth: 80},
+        {id: 'quantity', name: 'Quantity', field: 'quantity', cssClass: 'right-align',
+		headerCssClass: 'right-align'},
+        {id: 'last-trade', name: 'Last Trade', field: 'lastTrade', cssClass: 'right-align',
+		headerCssClass: 'right-align',  formatter: formatCurrency},
+        {id: 'market-value', name: 'Market Value', field: 'marketValue', cssClass: 'right-align',
+		headerCssClass: 'right-align', formatter: formatCurrency},
+        {id: 'price-paid', name: 'Price Paid', field: 'pricePaid', cssClass: 'right-align',
+		headerCssClass: 'right-align', formatter: formatCurrency},
+        {id: 'total-cost', name: 'Total Cost', field: 'totalCost', cssClass: 'right-align',
+		headerCssClass: 'right-align', formatter: formatCurrency},
+        {id: 'gain', name: 'Gain', field: 'gain', cssClass: 'positive right-align',
+		headerCssClass: 'right-align', formatter: formatGainMoney},
+        {id: 'gain-percent', name: 'Gain %', field: 'gainPercent', cssClass: 'positive right-align',
+		headerCssClass: 'right-align', formatter: formatGainPercent}
     ];
 
     // Essential Options
@@ -63,8 +76,8 @@
         rowHeight: 32
     };
 
-    // Preparing the Rows and Columns based on Window size
-	// These are calculated each time the window resizes
+    /* Preparing the Rows and Columns based on Window size
+    These are calculated each time the window resizes */
     function getRowHeight() {
         var windowWidth, rowHeight;
 
@@ -79,9 +92,9 @@
         windowWidth = $(window).width();
         newColumns = [];
 
-        /* Column priorities */
+        // Column priorities
         columnPriorities = [3, 1, 1, 1, 1, 2, 3, 3, 2];
-        // By default all columns will be shown
+        // Note: By default all columns will be shown
         maxPriority = 3;
         if (windowWidth < 500) {
             maxPriority = 1;
@@ -100,33 +113,33 @@
         return newColumns;
     }
 
-    // Display window size on window resize
+    /* Display window size on window resize */
     function displayWindowSize() {
         var win = $(window);
-        $('.window-size').html("(" + win.width() + ", " + win.height() + ")");
+        $('.window-size').html('(' + win.width() + ', ' + win.height() + ')');
         //console.log(win.height());
     }
 
 
     function resizeGridContainer() {
-        $("#positions-table").css("height", ($(window).height() - 132) + "px");
+        $('#positions-table').css('height', ($(window).height() - 132) + 'px');
     }
 
-    // Render table
+    /* Render table */
     function drawGrid() {
         var newColumns = getColumns();
         options.rowHeight = getRowHeight();
         resizeGridContainer();
-        grid = new Slick.Grid("#positions-table", data, newColumns, options);
+        grid = new Slick.Grid('#positions-table', data, newColumns, options);
         grid.onClick.subscribe(function (e, args) {
             var rowName = data[args.row].security;
             //Display
-            $("#selected-position").text(rowName);
+            $('#selected-position').text(rowName);
         });
         //grid.resizeCanvas();
     }
 
-    $(window).on("resize", function () {
+    $(window).on('resize', function () {
         drawGrid();
         displayWindowSize();
     });
